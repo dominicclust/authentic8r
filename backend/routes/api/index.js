6352579e246e2069843fdc9ed16b110a16c53194
dominicclust <dclust34@gmail.com>
2022-05-26 15:01:08 -0500

    Phase 3 complete

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,6 +4,28 @@ router.post('/test', (req, res) => {
     res.json({ requestBody: req.body })
 })
 
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie } = require('../../utils/auth');
+const { User } = require('../../db/models')
 
+router.get('/set-token-cookie', asyncHandler(async (_req, res) => {
+    const user = await User.findOne({
+        where: {
+            username: 'iEmmaDemo'
+        }
+    });
+    setTokenCookie(res, user);
+    return res.json({ user });
+}));
+
+const { restoreUser } = require('../../utils/auth');
+router.get('/restore-user', restoreUser, (req, res) => {
+    return res.json(req.user);
+});
+
+const { requireAuth } = require('../../utils/auth');
+router.get('/require-auth', requireAuth, (req, res) => {
+    return res.json(req.user);
+});
 
 module.exports = router;
