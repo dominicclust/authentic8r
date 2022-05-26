f5144c8a65e85a1c83cdca17d6db3856f5a16d1c
dominicclust <dclust34@gmail.com>
2022-05-26 16:13:51 -0500

    Almost done with the backend.

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,31 +1,38 @@
 const router = require('express').Router()
+const sessionRouter = require('./session');
+const usersRouter = require('./users');
+
+router.use('/session', sessionRouter);
+router.use('/users', usersRouter);
 
 router.post('/test', (req, res) => {
     res.json({ requestBody: req.body })
 })
 
-const asyncHandler = require('express-async-handler');
-const { setTokenCookie } = require('../../utils/auth');
-const { User } = require('../../db/models')
+// TEST CODE
 
-router.get('/set-token-cookie', asyncHandler(async (_req, res) => {
-    const user = await User.findOne({
-        where: {
-            username: 'iEmmaDemo'
-        }
-    });
-    setTokenCookie(res, user);
-    return res.json({ user });
-}));
+// const asyncHandler = require('express-async-handler');
+// const { setTokenCookie } = require('../../utils/auth');
+// const { User } = require('../../db/models')
 
-const { restoreUser } = require('../../utils/auth');
-router.get('/restore-user', restoreUser, (req, res) => {
-    return res.json(req.user);
-});
+// router.get('/set-token-cookie', asyncHandler(async (_req, res) => {
+//     const user = await User.findOne({
+//         where: {
+//             username: 'iEmmaDemo'
+//         }
+//     });
+//     setTokenCookie(res, user);
+//     return res.json({ user });
+// }));
 
-const { requireAuth } = require('../../utils/auth');
-router.get('/require-auth', requireAuth, (req, res) => {
-    return res.json(req.user);
-});
+// const { restoreUser } = require('../../utils/auth');
+// router.get('/restore-user', restoreUser, (req, res) => {
+//     return res.json(req.user);
+// });
+
+// const { requireAuth } = require('../../utils/auth');
+// router.get('/require-auth', requireAuth, (req, res) => {
+//     return res.json(req.user);
+// });
 
 module.exports = router;
